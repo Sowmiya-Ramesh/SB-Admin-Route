a2e5d0b55ef45afa87cab5d97f194b6edb19bb91
Sowmiya-Ramesh <miyaramesh9944@gmail.com>
2021-10-20 13:07:52 +0530

    first commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import SideBar from "./SideBar.js";
+// import Footer from "./Footer.js";
+// import Projects from "./Projects.js";
+// import Earnings from "./Earnings";
+import NavBar from "./NavBar.js";
+import Users from "./Users.js";
+import DashBoard from "./DashBoard";
+import Profiles from "./Profiles";
+import CreateProfile from "./CreateProfile.js";
+import EditProfile from "./EditProfile.js";
+//import EditUser from "./EditUser.js";
+import CreateUser from "./CreateUser";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" id="page-top">
+      <div id="wrapper">
+        <SideBar />
+
+        <div id="content-wrapper" className="d-flex flex-column">
+          <div id="content">
+            <NavBar />
+            <Router>
+              <Switch>
+                <Route exact path="/" component={DashBoard} />
+                <Route exact path="/profiles" component={Profiles} />
+                <Route exact path="/create-profile" component={CreateProfile} />
+                <Route exact path="/edit-profile/:id" component={EditProfile} />
+                <Route exact path="/users" component={Users} />
+                <Route exact path="/create-user" component={CreateUser} />
+                {/* <Route path="/edit-user/:id" component={EditUser} /> */}
+                {/* <Route path="/add" component={AddProduct} /> */}
+              </Switch>
+            </Router>
+          </div>
+        </div>
+      </div>
+
+      <a className="scroll-to-top rounded" href="#page-top">
+        <i className="fas fa-angle-up"></i>
+      </a>
     </div>
   );
 }
-
-export default App;
---
+++ my-app/src/CreateUser.js
@@ -0,0 +1,4 @@
+export default function CreateUser() {
+    return <div>Create User here ...!! </div>;
+  }
+  
